4f72da69de8bfee55d74e4654733e415ad7368f6
Diogo Ferreira <diogocraft.2@gmail.com>
2023-02-12 23:33:55 -0300

    feat(constants): adicionadas constantes para importações de temas, imagens, ícones e estilizações

---
+++ constants/icons.ts
@@ -0,0 +1,17 @@
+import search from "../assets/icons/search_icon.png";
+import plus from "../assets/icons/plus_icon.png";
+import chevron from "../assets/icons/chevron_icon.png";
+import user from "../assets/icons/user_icon.png";
+import cart from "../assets/icons/cart_icon.png";
+import dashboard from "../assets/icons/dashboard_icon.png";
+import menu from "../assets/icons/menu_icon.png";
+
+export default {
+  search,
+  plus,
+  chevron,
+  user,
+  cart,
+  dashboard,
+  menu
+};
---
+++ constants/images.ts
@@ -0,0 +1,11 @@
+import sofa from "../assets/images/sofa.png";
+import redChair from "../assets/images/red_chair.png";
+import greenChair from "../assets/images/green_chair.png";
+import whiteChair from "../assets/images/white_chair.png";
+
+export default {
+  sofa,
+  redChair,
+  greenChair,
+  whiteChair
+};
