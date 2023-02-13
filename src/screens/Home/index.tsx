b9eb9f2c2c972f86f3f849575bcd8d7f6378687e
Diogo Ferreira <diogocraft.2@gmail.com>
2023-02-13 00:05:23 -0300

    feat(screens): `Home` implementado header da aplicação

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,76 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+  Image
+} from 'react-native';
+
+import { images, icons, COLORS, FONTS, SIZES } from "../../../constants";
+
+import { getStatusBarHeight } from "react-native-status-bar-height";
 
 const Home = () => {
+
+  function renderHeader() {
+    return (
+      <View style={{ marginTop: getStatusBarHeight(), paddingHorizontal: SIZES.padding }}>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity
+            style={{ flex: 1 }}
+            onPress={() => {}}
+          >
+            <Image
+              source={icons.menu}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25
+              }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{ flex: 1, alignItems: "flex-end" }}
+            onPress={() => {}}
+          >
+            <Image
+              source={icons.cart}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Home screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {renderHeader()}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: COLORS.white
   },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9
+  }
 });
 
 export default Home;
