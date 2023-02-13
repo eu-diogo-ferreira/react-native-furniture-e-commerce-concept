b06460f3aa19d0fb057bd530fd3dd699e62958a6
Diogo Ferreira <diogocraft.2@gmail.com>
2023-02-13 00:38:08 -0300

    feat(app): `App` nova fontes importadas do gogole fonts adicionadas ao state da aplicação, no loading inicial

--- App.tsx
+++ App.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+  Roboto_900Black
+} from "@expo-google-fonts/roboto";
+
 import { Home, ItemDetail } from "./src/screens";
 
 const theme = {
@@ -15,6 +22,16 @@ const theme = {
 const Stack = createStackNavigator();
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Roboto_900Black
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
