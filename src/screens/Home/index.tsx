8e2078300982b9f1b1ee601f26b6b66581a31535
Diogo Ferreira <diogocraft.2@gmail.com>
2023-02-13 19:54:27 -0300

    feat(screens): `Home` adicionado scrool list de produtos, na tela inicial

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -45,7 +45,45 @@ const ScrollableTab = ({ tabList, selectedTab, onPress }) => {
   );
 };
 
-const Home = () => {
+const ScrollableCard = ({ navigation, productList }) => {
+  const renderCard = ({ item }) => (
+    <TouchableOpacity
+      style={{ marginLeft: SIZES.padding }}
+      onPress={() => navigation.navigate("ItemDetail", { itemInfo: item })}
+    >
+      <View style={{ width: SIZES.width / 2 }}>
+        <Image
+          source={item.image}
+          resizeMode="cover"
+          style={{ width: '100%', height: '100%', borderRadius: SIZES.radius * 2 }}
+        />
+
+        <View style={{ position: 'absolute', top: 15, left: '10%', right: '10%' }}>
+          <Text style={{ color: COLORS.lightGray2, ...FONTS.h3 }}>Furniture</Text>
+          <Text style={{ marginTop: SIZES.base, color: COLORS.white, ...FONTS.h2 }}>{item.productName}</Text>
+        </View>
+
+        <View style={{ position: 'absolute', bottom: 20, left: 30, borderRadius: 15, paddingVertical: 10, paddingHorizontal: 15, backgroundColor: COLORS.transparentLightGray }}>
+          <Text style={{ ...FONTS.h2 }}>$ {item.price.toFixed(2)}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={{ marginTop: SIZES.padding }}>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={productList}
+        renderItem={renderCard}
+        keyExtractor={(item) => item.productId}
+      />
+    </View>
+  )
+};
+
+const Home = ({ navigation }) => {
   const [tabList, setTabList] = useState([
     {
       id: 0,
@@ -235,6 +273,16 @@ const Home = () => {
         selectedTab={selectedTab}
         onPress={(item) => setSelectedTab(item)}
       />
+
+      <View style={{ flex: 1 }}>
+        <ScrollableCard
+          navigation={navigation}
+          productList={selectedTab.productList}
+        />
+      </View>
+
+      {/* coupon */}
+
     </SafeAreaView>
   );
 }
