d2cb1b4edd8001db2e659bc9d86aaf5662443770
Diogo Ferreira <diogocraft.2@gmail.com>
2023-02-13 20:15:32 -0300

    feat(screens): `Home` finalizado card com cupom de desconto

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -251,7 +251,7 @@ const Home = ({ navigation }) => {
         </View>
       </View>
     );
-  };
+  }
 
   function renderTitle(title: string) {
     return (
@@ -260,6 +260,74 @@ const Home = ({ navigation }) => {
         <Text style={{ color: COLORS.black, ...FONTS.largeTitle }}>{title}</Text>
       </View>
     );
+  }
+
+  function renderPromotionalCard() {
+    return (
+      <View
+        style={[styles.shadow, {
+          flexDirection: 'row',
+          marginHorizontal: SIZES.padding,
+          padding: SIZES.radius,
+          height: 110,
+          borderRadius: 20,
+          backgroundColor: COLORS.white
+        }]}
+      >
+        <View
+          style={{
+            width: 50,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: COLORS.lightGray2,
+            borderRadius: 20
+          }}
+        >
+          <Image
+            source={images.sofa}
+            resizeMode="contain"
+            style={{
+              width: "60%",
+              height: "60%"
+            }}
+          />
+        </View>
+
+        <View style={{ flex: 1, marginLeft: SIZES.radius, justifyContent: 'center' }}>
+          <Text style={{ ...FONTS.h2 }}>Special Offer</Text>
+          <Text style={{ ...FONTS.body3 }}>Adding to your cart</Text>
+        </View>
+
+        <View
+          style={{
+            marginRight: SIZES.radius,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: COLORS.primary,
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '70%',
+              width: 40,
+              borderRadius: 10
+            }}
+            onPress={() => console.log('promo')}
+          >
+            <Image
+              source={icons.chevron}
+              resizeMode="contain"
+              style={{
+                width: '50%',
+                height: '50%'
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
   }
 
   return (
@@ -281,8 +349,9 @@ const Home = ({ navigation }) => {
         />
       </View>
 
-      {/* coupon */}
-
+      <View style={{ height: "19%", justifyContent: "flex-end", marginBottom: (SIZES.padding / 4) }}>
+        {renderPromotionalCard()}
+      </View>
     </SafeAreaView>
   );
 }
