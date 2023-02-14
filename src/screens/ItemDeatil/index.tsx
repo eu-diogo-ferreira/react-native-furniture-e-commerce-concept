3ad98113b667c41ccc6c45f47c9e6d8a326a0c26
Diogo Ferreira <diogocraft.2@gmail.com>
2023-02-13 22:45:46 -0300

    feat(screens): `ItemDetail` desenvolvida tela para exibição detalhada de produtos

--- src/screens/ItemDeatil/index.tsx
+++ src/screens/ItemDeatil/index.tsx
@@ -1,20 +1,232 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image
+} from 'react-native';
+import { COLORS, FONTS, icons, SIZES } from '../../../constants';
 
-const ItemDetail = () => {
+const ItemDetail = ({ route, navigation }) => {
+
+  function renderHeader() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 2,
+          marginHorizontal: SIZES.padding
+        }}
+      >
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: 'flex-start',
+              justifyContent: 'center'
+            }}
+            onPress={() => console.log('menu')}
+          >
+            <Image
+              source={icons.menu}
+              resizeMode="contain"
+              style={{
+                tintColor: COLORS.white,
+                width: 25,
+                height: 25
+              }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: 'flex-end',
+              justifyContent: 'center'
+            }}
+            onPress={() => console.log('search')}
+          >
+            <Image
+              source={icons.search}
+              resizeMode="contain"
+              style={{
+                tintColor: COLORS.white,
+                width: 25,
+                height: 25
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  function renderInfo() {
+    const { itemInfo } = route.params;
+
+    if (!itemInfo) {
+      return (
+        <View>
+            <Text>Item not found</Text>
+        </View>
+      )
+    }
+
+    return (
+      <ImageBackground
+        source={itemInfo.image}
+        resizeMode="cover"
+        style={{ width: '100%', height: '100%' }}
+      >
+        {renderHeader()}
+
+        <View
+          style={{
+            position: 'absolute',
+            top: '45%',
+            left: '15%',
+            borderRadius: 80,
+            backgroundColor: COLORS.transparentLightGray1,
+            height: 40,
+            width: 40,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <View
+            style={{
+              borderRadius: 20,
+              backgroundColor: COLORS.blue,
+              width: 10,
+              height: 10
+            }}
+          />
+        </View>
+
+        <View
+          style={{
+            position: 'absolute',
+            top: '43%',
+            left: '30%',
+            flexDirection: 'row',
+            padding: SIZES.radius * 1.5,
+            backgroundColor: COLORS.transparentLightGray1,
+            width: '50%',
+            borderRadius: 10
+          }}
+        >
+          <View style={{ flex: 2 }}>
+            <Text style={{ color: COLORS.darkGray, ...FONTS.h3 }}>
+              {itemInfo.productName}
+            </Text>
+          </View>
+
+          <View style={{ flex: 1.5, alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+            <Text style={{ color: COLORS.darkGreen, ...FONTS.h3 }}>
+              $ {itemInfo.price.toFixed(2)}
+            </Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            position: 'absolute',
+            bottom: '20%',
+            left: SIZES.padding,
+            right: SIZES.padding
+          }}
+        >
+          <Text style={{ color: COLORS.lightGray2, ...FONTS.body2 }}>Furniture</Text>
+          <Text style={{ marginTop: SIZES.radius, color: COLORS.white, ...FONTS.h1 }}>{itemInfo.productName}</Text>
+        </View>
+      </ImageBackground>
+    )
+  }
+
+  function renderFooter() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          bottom: '5%',
+          left: SIZES.padding,
+          right: SIZES.padding,
+          flexDirection: 'row',
+          height: 70,
+          backgroundColor: COLORS.white,
+          borderRadius: 35
+        }}
+      >
+
+        <View
+          style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+          >
+            <Image
+              source={icons.dashboard}
+              style={{
+                tintColor: COLORS.gray,
+                width: 25,
+                height: 25
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <TouchableOpacity
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 50,
+              height: 50,
+              borderRadius: 10,
+              backgroundColor: COLORS.primary
+            }}
+            onPress={() => console.log('add')}
+          >
+            <Image
+              source={icons.plus}
+              style={{
+                tintColor: COLORS.white,
+                width: 20,
+                height: 20
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <TouchableOpacity
+            onPress={() => console.log('next')}
+          >
+            <Image
+              source={icons.user}
+              style={{
+                tintColor: COLORS.gray,
+                width: 25,
+                height: 25
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Item Detail</Text>
+    <View style={{ flex: 1, backgroundColor: COLORS.white }}>
+      {renderInfo()}
+      {renderFooter()}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default ItemDetail;
